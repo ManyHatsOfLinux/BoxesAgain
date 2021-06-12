3eeb2a22b0eae3cc8403543941e7e3b4fe44a153
zp <dale.borum.linux@gmail.com>
2021-06-11 20:50:44 -0400

    Second commit

---
+++ Scripts/Gameplay/BlockSpace/Block.gd
@@ -0,0 +1,124 @@
+extends Area2D
+
+
+var color : int 
+var paralyzed : bool = false 
+var DeathTimer
+var RealDeathTimer 
+var lastInLine : bool = false
+var ReadyToDie : bool = false
+var falling : bool = false
+var BlockSize : int = 0 
+var fallspeed = 2
+
+var ShouldFall = false
+
+var x_adj_neighbors : Array = []
+var y_adj_neighbors : Array = []
+
+onready var mySprite = get_node("Sprite")
+
+signal Paralyze(block)
+
+
+
+func _ready():
+	#print("BlockReady")
+	pass
+
+
+
+func _on_Block_area_entered(area):
+	#print(self, " has been Area_Enterd")
+	#update_adj_neighbors()
+	#match_self()
+	pass
+
+
+func _paralyze():
+	if paralyzed == false:
+		
+		paralyzed = true
+	
+		#dim the block
+		mySprite.modulate = Color(1,1,1,0.5)
+		
+		#send signal to BlockSpace that this block was matched
+		emit_signal("Paralyze", self)
+
+
+
+func fall():
+	self.falling = true
+	self.position = Vector2(self.position.x,self.position.y + fallspeed)
+	pass
+
+
+func update_adj_neighbors():
+	#reset neighbor variables
+	x_adj_neighbors = []
+	y_adj_neighbors = []
+	
+	#loop though all blocks touching this block.
+	for neighbor in get_overlapping_areas():
+		
+		#if neighbor is on the same X axis and share the same color
+		if (neighbor.position.x == self.position.x) && (neighbor.color == self.color):
+			if neighbor.falling == false:
+				y_adj_neighbors.append(neighbor)
+		
+		#if neighbor is on the same Y axis and share the same color
+		elif (neighbor.position.y == self.position.y) && (neighbor.color == self.color):
+			if neighbor.falling == false:
+				x_adj_neighbors.append(neighbor)
+		
+	get_node("xNeighbors").set_text( "X:" + str(x_adj_neighbors.size()))
+	get_node("yNeighbors").set_text( "Y:" + str(y_adj_neighbors.size()))
+
+
+
+
+func match_self():
+	if self.falling == false:
+		if x_adj_neighbors.size() == 2:
+			self._paralyze()
+			for x in x_adj_neighbors:
+				x._paralyze()
+	
+		if y_adj_neighbors.size() == 2:
+			self._paralyze()
+			for y in y_adj_neighbors:
+				y._paralyze()
+
+
+
+
+func killTimer(time: float, RealTime: float):
+	DeathTimer = Timer.new()
+	DeathTimer.connect("timeout", self, "_on_DeathTimer_Timeout_")
+	DeathTimer.set_wait_time(time)
+	add_child(DeathTimer)
+	
+	
+	RealDeathTimer = Timer.new()
+	RealDeathTimer.connect("timeout", self, "_on_RealDeathTimer_Timeout_")
+	RealDeathTimer.set_wait_time(RealTime)
+	add_child(RealDeathTimer)
+	
+	DeathTimer.start()
+	RealDeathTimer.start()
+
+func _on_DeathTimer_Timeout_():
+	mySprite.modulate = Color(1,1,1,0)
+	DeathTimer.stop()
+	ReadyToDie = true
+
+
+
+func _on_RealDeathTimer_Timeout_():
+	queue_free()
+
+func _process(delta):
+	update_adj_neighbors()
+	match_self()
+	pass
---
+++ Scripts/Gameplay/BlockSpace/BlockSpace.gd
@@ -0,0 +1,241 @@
+extends Node
+
+
+export (int) var BlockSize = 64
+export (int) var StartingRows = 7
+export (int) var BlockSpaceHeight = 12
+export (int) var BlockSpaceWidth = 6
+export (int) var DeathDelayMax = 1
+
+#multiplies with the number of blocks destroying
+export (float) var DeathDelay = .2
+
+var BlockParent = preload("res://Scenes/Gameplay/BlockSpace/Block.tscn")
+
+
+
+#
+var ParalyzedBlocks = []
+
+#
+var DyingBlocks = []
+
+
+
+#for the different Block Types
+var BlockTextures = [
+	preload("res://Assets/Textures/Blocks/Blue Piece.png"),
+	preload("res://Assets/Textures/Blocks/Green Piece.png")
+	#preload("res://Assets/Textures/Blocks/Light Green Piece.png"),
+	#preload("res://Assets/Textures/Blocks/Orange Piece.png"),
+	#preload("res://Assets/Textures/Blocks/Pink Piece.png"),
+	#preload("res://Assets/Textures/Blocks/Yellow Piece.png")
+	#preload("res://Assets/Textures/Blocks/Black Piece.png")
+]
+
+
+#for sorting blocks later
+class BlockSort:
+	#return true if item 1 should be first in the list, otherwise return false. 
+	static func sort_top_to_bottom_left_to_right(block1,block2):
+		#if block is higher up
+		if block1.position.y < block2.position.y :
+			return true
+		#if block is on same row
+		elif block1.position.y == block2.position.y:
+			#block is same row, but further to the left.
+			if block1.position.x < block2.position.x:
+				return true
+			#block was further to the right
+			else: 
+				return false
+		#block was higher.
+		else: 
+			return false
+		
+	static func sort_buttom_to_top_left_to_right(block1,block2):
+		#if block is lower 
+		if block1.position.y > block2.position.y :
+			return true
+		#if block is on same row
+		elif block1.position.y == block2.position.y:
+			#block is same row, but further to the left.
+			if block1.position.x < block2.position.x:
+				return true
+			#block was further to the right
+			else: 
+				return false
+		#block was higher.
+		else: 
+			return false
+		
+
+
+
+
+#to spawn blocks
+func spawn_block(x: int, y: int, color: int) -> void:
+	
+	var block = BlockParent.instance()
+	block.color = color
+	block.get_node("Sprite").set_texture(BlockTextures[color])
+	add_child(block)
+	block.add_to_group("Blocks")
+	
+	#adjust for size of block
+	x = (x*BlockSize)
+	y = (y*BlockSize) * -1
+	
+	#set new position
+	block.position = Vector2(x,y)
+	block.BlockSize = BlockSize
+
+	#connect to block signal
+	block.connect("Paralyze", self, "_on_block_paralyzed")
+
+
+
+
+
+
+
+func spawn_starting_blocks() -> void:
+	for x in BlockSpaceWidth:
+		for y in StartingRows:
+			var rand = floor(rand_range(0,BlockTextures.size()));
+			spawn_block(x,y,rand)
+
+
+#to update list of blocks to keep track of match count
+func _on_block_paralyzed(block):
+	ParalyzedBlocks.append(block)
+	ParalyzedBlocks.sort_custom(BlockSort, "sort_top_to_bottom_left_to_right")
+	#print("Block Paralized")
+
+
+
+#start blocks kill timer.
+func MarkTheDead():
+	var DeathDelayinc : float = DeathDelay 
+	
+	if ParalyzedBlocks.size() > 0:
+		for x in range(ParalyzedBlocks.size()):
+			var block = ParalyzedBlocks[x]
+			var RealDeathDelay = (DeathDelay*2) + (DeathDelay*ParalyzedBlocks.size())
+			
+			
+			#incremebent delay incremembet if not past the max
+			if DeathDelayinc < DeathDelayMax:
+				DeathDelayinc = DeathDelay + DeathDelayinc
+
+			#
+			if RealDeathDelay > DeathDelayMax:
+				RealDeathDelay = (DeathDelayMax)
+
+			block.killTimer(DeathDelayinc, RealDeathDelay)
+	
+		print("Marked ", ParalyzedBlocks.size(), " Blocks.")
+		ParalyzedBlocks = []
+
+
+
+#returns list of blocks above Block1
+func getBlocksAbove(Block1) -> Array:	
+	
+	#store all blocks in array
+	var BlockList = (get_tree().get_nodes_in_group("Blocks"))
+	BlockList.sort_custom(BlockSort, "sort_buttom_to_top_left_to_right")
+	
+	#empty array to list all blocks above Block1
+	var BlocksAbove: Array = []
+	
+	#loop thorugh all blocks and test if on same x axis, if so
+	#check if higher on the y axis 
+	for Block2 in BlockList:
+		#Must be on same X axis
+		if Block1.position.x == Block2.position.x :
+			#if block2 y position is lower, its higher on screen.
+			if Block2.position.y < Block1.position.y:
+				BlocksAbove.append(Block2)
+	#return list of blocks
+	return BlocksAbove
+
+#returns list of blocks above Block1
+func getBlocksBelow(Block1) -> Array:	
+	
+	#store all blocks in array
+	var BlockList = (get_tree().get_nodes_in_group("Blocks"))
+	BlockList.sort_custom(BlockSort, "sort_buttom_to_top_left_to_right")
+	
+	#empty array to list all blocks below Block1
+	var BlocksBelow: Array = []
+	
+	#loop thorugh all blocks and test if on same x axis, if so
+	#check if higher on the y axis 
+	for Block2 in BlockList:
+		#Must be on same X axis
+		if Block1.position.x == Block2.position.x :
+			#if block2 y position is higher, its lower on screen.
+			if Block2.position.y > Block1.position.y:
+				BlocksBelow.append(Block2)
+	#return list of blocks
+	return BlocksBelow
+
+
+func should_fall(Block):
+	var BlocksBelow : Array = getBlocksBelow(Block) 
+	var sizebelow : int = (BlocksBelow.size()*BlockSize)
+	
+	if sizebelow < (Block.position.y*-1) :
+		return true
+	else:
+		return false
+
+
+#marks blocks that need fall this frame
+func check_for_falling():
+	
+	#store all blocks in array
+	var BlockList : Array = (get_tree().get_nodes_in_group("Blocks"))
+	BlockList.sort_custom(BlockSort, "sort_buttom_to_top_left_to_right")
+	
+	#keep track of blocks already checked
+	var BlocksAlreadyFallen : Array = []
+	
+	for Block in BlockList:
+		if Block.paralyzed == false && BlocksAlreadyFallen.has(Block) == false: 
+			#if block not already falling, And has no lower neighbors.
+			if  should_fall(Block) == true :
+				Block.fall()
+				BlocksAlreadyFallen.append(Block)
+			
+			
+				#block is fallen mark all blocks above
+				var BlocksAbove = getBlocksAbove(Block)
+
+				for BlockX in BlocksAbove:
+						BlockX.fall()
+						BlocksAlreadyFallen.append(Block)
+						
+		
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("BoardReady")
+	randomize()
+	
+	#spawn_block(0,1,1)
+	#spawn_block(0,2,1)
+	#spawn_block(1,1,1)
+	spawn_starting_blocks()
+
+func _process(delta):
+	#print("BoardProcessStart")
+	#all blocks that are matching get 
+	MarkTheDead()
+
+	#check for falling
+	check_for_falling()
+
